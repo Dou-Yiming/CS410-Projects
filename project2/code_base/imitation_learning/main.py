85883d94778c2a4a230695c474b51099d02450c6
Iridoudou <2534936416@qq.com>
2021-11-21 19:20:33 +0800

    support ckpt

--- project2/code_base/imitation_learning/main.py
+++ project2/code_base/imitation_learning/main.py
@@ -25,7 +25,6 @@ def parse_args():
     parser = argparse.ArgumentParser(description='n-gram language model')
     parser.add_argument('--data_path', dest='data_path',
                         help='path of dataset',
-                        # default='E:/Datasets/LUX_ai/DATA/top/', type=str)
                         default='E:/Datasets/LUX_ai/DATA/2M/', type=str)
     parser.add_argument('--config_path', dest='config_path',
                         help='path of config',
@@ -50,33 +49,42 @@ def main(args, cfg):
     print('obs:', len(obses), 'sample:', len(samples))
     labels = [sample[-1] for sample in samples]
 
-    model = LuxNet(cfg=cfg.MODEL)
-    # model=LuxNetTransformer()
+    if cfg.MODEL.CKPT == '':
+        model = LuxNet(cfg=cfg.MODEL)
+    else:
+        print("Loading checkpoint from {}".format(cfg.MODEL.CKPT))
+        model = torch.jit.load(cfg.MODEL.CKPT)
     train, val = train_test_split(
         samples, test_size=0.1, random_state=args.seed, stratify=labels)
     train_loader = DataLoader(
         LuxDataset(obses, train),
         batch_size=cfg.TRAIN.BATCH_SIZE,
         shuffle=True,
-        num_workers=4
+        num_workers=0
     )
     val_loader = DataLoader(
         LuxDataset(obses, val),
         batch_size=cfg.TEST.BATCH_SIZE,
         shuffle=False,
-        num_workers=4
+        num_workers=0
     )
     dataloaders_dict = {"train": train_loader, "val": val_loader}
-    print("Data loaded Train: {}, Val: {}".format(len(train_loader), len(val_loader)))
+    print("Data loaded Train: {}, Val: {}".format(
+        len(train_loader), len(val_loader)))
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(
-        model.parameters(), lr=cfg.TRAIN.OPTIMIZER.BASE_LR, weight_decay=1e-8)
-    t_0 = len(train) // cfg.TRAIN.BATCH_SIZE * 2
+    if cfg.TRAIN.OPTIMIZER.TYPE == 'AdamW':
+        optimizer = optim.AdamW(
+            model.parameters(), lr=cfg.TRAIN.OPTIMIZER.BASE_LR,
+            weight_decay=1e-8)
+    else:
+        optimizer = optim.SGD(
+            model.parameters(), lr=cfg.TRAIN.OPTIMIZER.BASE_LR,
+            momentum=0.9, weight_decay=1e-9)
     scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
         optimizer,
-        T_0=t_0,
+        T_0=len(train) // cfg.TRAIN.BATCH_SIZE * 2,
         T_mult=2,
-        eta_min=cfg.TRAIN.OPTIMIZER.MIN_LR)
+        eta_min=cfg.TRAIN.SCHEDULER.MIN_LR)
 
     train_model(model, dataloaders_dict, criterion,
                 optimizer, scheduler,
